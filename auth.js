aae71eb3dd2dfddb94f158fa4f38443eeeadb820
ProgramasCamargo <emailscamargo@gmail.com>
2025-06-18 13:57:00 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,8 +1,28 @@
 // auth.js
 
 const usuariosAtivos = {
-  "Joao_emmenderfer": "51235asd",
-  "maria": "abcd"
+  "Guilherme": "Guilherme12345",
+  "Alex": "Alex12345",
+  "Katlrin": "Katlrin12345",
+  "Eduarda": "Eduarda12345",
+  "Brayan": "Brayan12345",
+  "Maykon": "Maykon12345",
+  "Lucas": "Lucas12345",
+  "Osvaldo": "Osvaldo12345",
+  "Paulo": "Paulo12345",
+  "Fossile": "Fossile12345",
+  "Frigheto": "Frigheto12345",
+  "Deivison": "Deivison12345",
+  "Thiago": "Thiago12345",
+  "Hernane": "Hernane12345",
+  "Silvio": "Silvio12345",
+  "Anna": "Anna12345",
+  "Sergio": "Sergio12345",
+  "Joao": "Joao12345",
+  "Anderson": "Anderson12345",
+  "Kaleu": "Kaleu12345",
+  "Geison": "Geison12345",
+  "Peterson": "Peterson12345"
 };
 
 function autenticar(usuario, senha) {
